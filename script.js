18aa6c000071e40165e08ae670c78f45469d666d
Vanessa Constant <vaconstant@gmail.com>
2022-09-02 12:20:09 -0400

    Able to add destination cards

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
+// Getting all the input elements
 
-const form = document.getElementById("destination_form");
-console.log(form);
+const destinationInput = document.getElementById('destination_name');
+const locationInput = document.getElementById('location');
+const photoUrlInput = document.getElementById('photo_url');
+const descriptionInput = document.getElementById('description');
 
-const destinationInput = form.elements['destination_name'];
+// Getting the submit button for the click event
+const submit = document.getElementById('submit-btn');
 
-const locationInput = form.elements['location'];
-const img_urlInput = form.elements['photo_url'];
-const descriptionInput = form.elements['description'];
+// Function that will created each destination card
+function addNewDestinationCard(){
 
-let destination = destinationInput.value;
-console.log(destination);
+    // getting the values inside of all the input elements.
+    // Need to grab them in here because there is no values until the user hits submit
+    const destination_name = destinationInput.value;
+    const location = locationInput.value;
+    const photoUrl = photoUrlInput.value;
+    const description = descriptionInput.value;
+    
+    //grabbing the area where all the destination will be displayed 
+    const destination_results = document.getElementById("wishlist_result");
+
+    // creating the div for each destination card
+    const destinationCard = document.createElement('div');
+    destinationCard.setAttribute('class', 'destination_card');
+
+    // creating the outline of the card
+    const card = `
+    <h3>${destination_name}</h3>
+    <img src='${photoUrl}'>
+    <p>${location}</p>
+    <p>${description}</p>`;
+
+
+    destinationCard.innerHTML = card;
+    destination_results.append(destinationCard);
+    
+}
+
+// The event listener
+
+submit.addEventListener('click',(event) =>{
+    event.preventDefault();
+    addNewDestinationCard();
+});
